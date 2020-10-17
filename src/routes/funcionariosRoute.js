933cf6fca650e1fcb6bdbe9a6321ab37230b4ac1
marilliaalves <mmarillialves@gmail.com>
2020-10-17 16:51:56 -0300

    atualizações

--- src/routes/funcionariosRoute.js
+++ src/routes/funcionariosRoute.js
@@ -2,3 +2,12 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controller/funcionariosController");
 
+router.get("/", controller.getAllFuncionarios);
+
+router.get("/funcionarios/:id", controller.getByIdFuncionarios);
+
+router.post("/", controller.postFuncionarios);
+
+router.delete("/:id", controller.deleteFuncionarios);
+
+module.exports = router; 
--- src/routes/livrosRoute.js
+++ src/routes/livrosRoute.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controller/funcionariosController");
+const controller = require("../controller/livrosController");
 
+router.get("/", controller.getAll);
+router.get("/:id", controller.getById);
+
+router.post("/", controller.postLivros);
+
+router.delete("/:id", controller.deleteLivros);
+
+module.exports = router; 
